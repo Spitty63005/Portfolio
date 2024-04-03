f79d148f20f50ecadcd2fd857c8d22474f49a1f7
Tommy <manningtw@oldcolony.info>
2024-04-03 13:37:05 -0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let freshman = document.getElementById("freshman");
-let sophmore = document.getElementById("sophmore");
+let sophomore = document.getElementById("sophmore");
 let junior = document.getElementById("junior");
 let senior = document.getElementById("senior");
 let currentGrade = freshman;
@@ -7,27 +7,38 @@ let currentGrade = freshman;
 let academic = document.getElementById("acaWrk");
 let shop = document.getElementById("shopWrk");
 let related = document.getElementById("relWrk");
+let ddm = document.getElementById("ddm");
+
+let close = document.getElementById("closeBtn")
+let open = document.getElementById("openBtn");
+
+let pageReference = document.getElementById("title");
+
+
 
 function setCurrentgrade(grade) 
 {
-  if(grade == "freshman")
+  console.log(grade)
+  if(!(grade == currentGrade))
   {
-    setLinksWithGrade("Freshman");
+    if(grade == "freshman")
+    {
+      setLinksWithGrade("Freshman");
+    }
+    else if(grade == "sophomore") 
+    {
+      setLinksWithGrade("Sophomore");
+    }
+    else if(grade == "junior")
+    {
+      setLinksWithGrade("Junior");
+    }
+    else if(grade == "senior")
+    {
+      setLinksWithGrade("Senior");
+    }
   }
-  else if(grade == "sophmore") 
-  {
-    setLinksWithGrade("Sophmore");
-  }
-  else if(grade == "junior")
-  {
-    setLinksWithGrade("Junior");
-  }
-  else if(grade == "senior")
-  {
-    setLinksWithGrade("Senior");
-  }
-
-}
+};
 
 
 function setLinksWithGrade(gradeName)
@@ -35,15 +46,41 @@ function setLinksWithGrade(gradeName)
   academic.href = "htmls/Years/" + gradeName + "/academicWork.html";
   shop.href = "htmls/Years/" + gradeName + "/shopWork.html";
   related.href = "htmls/Years/" + gradeName + "/relatedWork.html";
+  ddm.href = "htmls/Years/" + gradeName + "/ddm.html";
+  goToDropDownLink(gradeName);
 }
 
+function goToDropDownLink(grade)
+{
+  window.location.href = "../" + grade + "/" + pageReference.textContent + ".html";
+}
+
+function openFullText()
+{
+  let fullText = document.getElementById("fullTextDimmed");
+  fullText.setAttribute('style', 'display: block;');
+}
+
+function closeFullText()
+{
+  let fullText = document.getElementById("fullTextDimmed");
+  fullText.style.display = "none";
+}
+
+open.addEventListener("click", function(){
+  openFullText();
+});
+
+close.addEventListener('click', function() {
+  closeFullText();
+});
 
 freshman.addEventListener('click', function() {
   setCurrentgrade('freshman');
 });
 
-sophmore.addEventListener('click', function() {
-  setCurrentgrade('sophmore');
+sophomore.addEventListener('click', function() {
+  setCurrentgrade('sophomore');
 });
 
 junior.addEventListener('click', function() {
